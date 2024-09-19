81374cf7ecb61fd9707f74836aeec8cdcd41c123
W0198745JC <w0198745@campus.nscc.ca>
2024-09-19 11:47:05 -0300

    Finished Exercise 1

--- Lab1-PaintShop.py
+++ Lab1-PaintShop.py
@@ -11,3 +11,20 @@
 
 #Purpose/Concepts: Input and output to screen, string concatentation, datatype casting, simple math operations
 
+print("Welcome to the Paint Shop Calculator")
+# how wide is the longer wall
+Wall1= input("what is the width of your long wall in ft?")
+#how wide is the shorter wall
+Wall2= input("what is the width of your short wall in ft?")
+#what is the height of the room
+Height = input("what is the height of your wall in ft?")
+#footage of wall 1
+Size1 = int(Wall1)*int(Height)
+#footage of wall 2
+Size2 = int(Wall2)*int(Height)
+# get the square footage of the room
+size = Size1*2 + Size2*2
+#divide by 150 and round to the nearest 1
+gallons=round(size/150)
+#print the needed amount
+print(("You will need to buy ")+str(gallons)+(" gallon(s) to paint your room"))
